9df6ec94a0f849f22c33349b99e43031de1a0795
Yaron <yaron.friedman@gmail.com>
2011-01-24 21:01:09 -0800

    add mini data

---
+++ pa2/data/mini/mini.e
@@ -0,0 +1,4 @@
+<s snum=1> A B C </s>
+<s snum=2> A B </s>
+<s snum=3> B C </s>
+<s snum=4> D F </s>
---
+++ pa2/data/mini/mini.f
@@ -0,0 +1,4 @@
+<s snum=1> X Y Z </s>
+<s snum=2> X Y </s>
+<s snum=3> Y W Z </s>
+<s snum=4> U V W </s>
