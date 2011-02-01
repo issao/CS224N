488f59eb127f1d64e30d8f13378463188fc359c9
yfriedman <yfriedman@yfriedman-macbookpro>
2011-01-31 22:18:02 -0800

    Generify fixed displacement distortion model so buckets can very. ALos minor cleanup

--- pa2/java/src/cs224n/wordaligner/FixedDisplacementDistortionModel.java
+++ pa2/java/src/cs224n/wordaligner/FixedDisplacementDistortionModel.java
@@ -2,13 +2,12 @@ package cs224n.wordaligner;
 
 import java.util.List;
 
-import cs224n.util.Counter;
 import cs224n.util.SentencePair;
 
 public class FixedDisplacementDistortionModel implements DistortionModel {
 
-  private Counter<Integer> distortionModelParams;
-  private Counter<Integer> trainingDistortionModelParams;
+  private static final int MAX_BUCKET = 10;
+  private static final double NULL_PROBABILITY = 0.2;
 
   @Override
   public void init(List<SentencePair> trainingPairs) {
@@ -21,16 +20,20 @@ public class FixedDisplacementDistortionModel implements DistortionModel {
   public double getFixedProbability(int englishLength, int englishPosition,
       int frenchLength, int frenchPosition) {
     if (englishPosition == -1) {
-      return 0.2;
+      return NULL_PROBABILITY;
     } else {
-      return (6 - bucket(englishLength, englishPosition, frenchLength, frenchPosition)) * 0.8 / 21;
+      return (MAX_BUCKET + 1 - bucket(englishLength, englishPosition,
+          frenchLength, frenchPosition))
+          * (1 -  NULL_PROBABILITY)
+          / ((MAX_BUCKET + 1) * (MAX_BUCKET + 2) / 2.0);
     }
   }
-  
+
   @Override
   public double getProbability(int englishLength, int englishPosition,
       int frenchLength, int frenchPosition) {
-    return getFixedProbability(englishLength, englishPosition, frenchLength, frenchPosition);
+    return getFixedProbability(englishLength, englishPosition, frenchLength,
+        frenchPosition);
   }
 
   private int bucket(int englishLength, int englishPosition, int frenchLength,
@@ -42,12 +45,12 @@ public class FixedDisplacementDistortionModel implements DistortionModel {
     assert englishPosition >= 0;
     int bucket = (int) Math.floor(Math.abs(englishPosition - frenchPosition
         * 1.0 * englishLength / frenchLength));
-    return Math.min(bucket, 5);
+    return Math.min(bucket, MAX_BUCKET);
   }
 
   @Override
   public void addFractionalCount(int englishLength, int englishPosition,
-      int frenchLength, int frenchPosition, double count) { 
+      int frenchLength, int frenchPosition, double count) {
   }
 
   @Override
