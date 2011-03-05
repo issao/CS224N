88cfaf456c5174ee22b22bf415a891f5cab27ab0
yfriedman <yfriedman@dhcp-172-19-102-135.mtv.corp.google.com>
2011-03-05 13:24:16 -0800

    bring in enough of pa1 to run ZipfChimeraInterpolatedTriGramModel

---
+++ final/java/src/cs224n/langmodel/EmpiricalNGramModel.java
@@ -0,0 +1,94 @@
+package cs224n.langmodel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import cs224n.util.Counter;
+
+public class EmpiricalNGramModel extends NGram {
+
+  private Set<String> lexicon;
+  private Map<List<String>, Counter<String>> rawCount;
+  private Map<List<String>, Double> totalCount;
+
+  public EmpiricalNGramModel(int n) {
+    super(n);
+    lexicon = new HashSet<String>();
+    rawCount = new HashMap<List<String>, Counter<String>>();
+    totalCount = new HashMap<List<String>, Double>();
+  }
+
+  @Override
+  public void train(Collection<List<String>> trainingSentences) {
+    for (List<String> sentence : trainingSentences) {
+      List<String> stoppedSentence = new ArrayList<String>(sentence);
+      for (int i = 0; i < n - 1; i++) {
+        stoppedSentence.add(0, START);
+      }
+      stoppedSentence.add(STOP);
+      for (int i = n - 1; i < stoppedSentence.size(); i++) {
+        lexicon.add(stoppedSentence.get(i));
+        addNgramCount(stoppedSentence, i);
+      }
+    }
+    for (List<String> prefix : rawCount.keySet()) {
+      totalCount.put(prefix, rawCount.get(prefix).totalCount());
+    }
+  }
+
+  private void addNgramCount(List<String> stoppedSentence, int index) {
+    assert index + 1 >= n;
+    List<String> prefix = getPrefix(stoppedSentence, index);
+    if (!rawCount.containsKey(prefix)) {
+      rawCount.put(prefix, new Counter<String>());
+    }
+    rawCount.get(prefix).incrementCount(stoppedSentence.get(index), 1.0);
+  }
+
+  @Override
+  public double getWordProbability(List<String> prefix, String word) {
+    assert prefix.size() == n - 1;
+    if (!rawCount.containsKey(prefix)) {
+      // Missing prefix, give uniform probability.
+      return 1.0 / (lexicon().size() + 1); 
+    }
+    return rawCount.get(prefix).getCount(word) / totalCount.get(prefix);
+  }
+
+  @Override
+  public Set<List<String>> knownPrefixes() {
+    return rawCount.keySet();
+  }
+
+  @Override
+  public Set<String> knownWords(List<String> prefix) {
+    assert prefix.size() == n - 1;
+    return rawCount.get(prefix).keySet();
+  }
+
+  @Override
+  protected Set<String> lexicon() {
+    return lexicon;
+  }
+  
+  public int getCount(List<String> prefix, String word) {
+    assert prefix.size() == n - 1;
+    if (!rawCount.containsKey(prefix)) {
+      return 0;
+    }
+    return (int) rawCount.get(prefix).getCount(word);
+  }
+
+  public Counter<String> getPrefixCounter(List<String> prefix) {
+    assert prefix.size() == n - 1;
+    if (!rawCount.containsKey(prefix)) {
+      return null;
+    }
+    return rawCount.get(prefix);
+  }
+}
---
+++ final/java/src/cs224n/util/IdentityCounter.java
@@ -0,0 +1,14 @@
+package cs224n.util;
+
+/**
+ * Convenience Extension of Counter to use an IdentityHashMap.
+ *
+ * @author Dan Klein
+ */
+public class IdentityCounter<E> extends Counter<E> {
+  static final long serialVersionUID = 1L;
+
+  public IdentityCounter() {
+    super(new MapFactory.IdentityHashMapFactory<E,Double>());
+  }
+}
