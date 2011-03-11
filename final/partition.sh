3f3c9e99eb06b281d043fea8bc5bbf2e08df8b84
Issao Fujiwara <issaofujiwara@gmail.com>
2011-03-10 22:06:03 -0800

    m

--- final/partition.sh
+++ final/partition.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#
+# After creating the file splits with ./create_train_and_tune_data.sh,
+# use this by runnign "cat splits | ./partition.sh"
+#
+# That will create a .tune and .train file for each *.all corpora file,
+# with the first 10% of lines and last 90% of lines, respectively.
 
 while read line
 do
   set $line
-  head -n $1 $3 > $3.tune
-  tail -n $2 $3 > $3.train
+  head -n $1 $3 > data/parsed/$3.tune
+  tail -n $2 $3 > data/parsed/$3.train
 done
