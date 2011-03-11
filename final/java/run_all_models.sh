0c93faacfc42bbdc2f3de3c3ecab351a1992ed2f
Issao Fujiwara <issaofujiwara@gmail.com>
2011-03-11 01:23:40 -0800

    m

--- final/java/run_all_models.sh
+++ final/java/run_all_models.sh
@@ -3,7 +3,7 @@
 # Train models for all the corpora we have obtained and evaluate each
 # corpora against each of these models.
 
-for m in $(cat ../model_list | tr "," " ")
+for m in $(cat ../models_list | tr "," " ")
 do
-  echo ./run -train $m -tes $(cat ../model_list)
+  ./run -train $m.train -valid $m.tune -test $(cat ../models_list)
 done
